f4f26e61fbcdf8f8370a4b712461717b24b9b5dc
Marat <mrmar99@icloud.com>
2022-02-24 16:33:32 +0300

    Deleted .js files

--- src/utils/consts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CREDIT_UNIT_IN_HOURS = exports.DISCIPLINE_ROUTE = exports.CURRICULUM_ROUTE = exports.STAFF_ROUTE = exports.NEWS_ROUTE = exports.HOME_ROUTE = void 0;
-exports.HOME_ROUTE = '/';
-exports.NEWS_ROUTE = '/news';
-exports.STAFF_ROUTE = '/staff';
-exports.CURRICULUM_ROUTE = '/curriculum';
-exports.DISCIPLINE_ROUTE = '/discipline/:id';
-exports.CREDIT_UNIT_IN_HOURS = 36;
--- src/utils/declensions.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.declensions = void 0;
-const declensions = (number, words) => words[(number % 100 > 4 && number % 100 < 20) ?
-    2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ?
-    Math.abs(number) % 10 : 5]];
-exports.declensions = declensions;
